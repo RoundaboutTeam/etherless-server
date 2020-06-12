15e5cd2cccfc7bea594590ce1ea1b1a133245c08
Antonio Z <antonio.zlatkovski438@gmail.com>
2020-06-12 12:47:54 +0200

    added ipfs to EventProcessor

--- src/facade/EventProcessor.ts
+++ src/facade/EventProcessor.ts
@@ -1,6 +1,7 @@
 import IEventProcessor from './IEventProcessor';
 import EthSmartManager from '../smart/EthSmartManager';
 import AwsManager from '../aws/AwsManager';
+import IpfsManager from '../ipfs/IpfsManager';
 import RunEventData from '../event/RunEventData';
 import DeployEventData from '../event/DeployEventData';
 
@@ -9,9 +10,12 @@ class EventProcessor implements IEventProcessor {
 
     private awsManager: AwsManager;
 
-    constructor(smartManager: EthSmartManager, awsManager: AwsManager) {
+    private ipfsManager: IpfsManager;
+
+    constructor(smartManager: EthSmartManager, awsManager: AwsManager, ipfsManager: IpfsManager) {
       this.smartManager = smartManager;
       this.awsManager = awsManager;
+      this.ipfsManager = ipfsManager;
 
       this.smartManager.onRun((data:RunEventData) => {
         this.processRunEvent(data);
