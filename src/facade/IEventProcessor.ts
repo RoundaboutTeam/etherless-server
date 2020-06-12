222a362436466d51f4785de85c4a6cf06f316234
Antonio Z <antonio.zlatkovski438@gmail.com>
2020-06-12 12:31:32 +0200

    added deploy functionality to IEventProcessor

--- src/facade/IEventProcessor.ts
+++ src/facade/IEventProcessor.ts
@@ -1,6 +1,8 @@
 import RunEventData from '../event/RunEventData';
+import DeployEventData from '../event/DeployEventData';
 
 export default interface IEventProcessor {
   // orchestrates the processing of run events
   processRunEvent(data: RunEventData): void;
+  processDeployEvent(data: DeployEventData): void;
 };
